c624ebd33b5e896a341811d10e48403317cdec1e
Corentin Bousquet <corentin.bousquet11@gmail.com>
2021-03-05 15:08:15 +0100

    ajout dossier

--- php/landing.php
+++ php/landing.php
@@ -1,47 +1,60 @@
+<?php 
+    session_start();
+    if(!isset($_SESSION['user'])){
+        header('Location:index.php');
+        die();
+    }
+?>
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Espace membre</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+        <div class="container">
+            <div class="col-md-12">
+                <?php 
+                        if(isset($_GET['err'])){
+                            $err = htmlspecialchars($_GET['err']);
+                            switch($err){
+                                case 'current_password':
+                                    echo "<div class='alert alert-danger'>Le mot de passe actuel est incorrect</div>";
+                                break;
+
+                                case 'success_password':
+                                    echo "<div class='alert alert-success'>Le mot de passe a bien été modifié ! </div>";
+                                break; 
+                            }
+                        }
+                    ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-</head>
-<body>
-    
-<div class="container">
-	<div class="row">
+                <div class="text-center">
+                        <h1 class="p-5">Bonjour  <?php echo $_SESSION['user']; ?> ! </h1>
+                        <hr />
+                        <a href="deconnexion.php" class="btn btn-danger btn-lg">Déconnexion</a>
+                        <!-- Button trigger modal -->
+                        
+                </div>
+            </div>
+        </div>    
 
-              <table id="mytable" class="table table-bordred table-striped">
-                   
-                   <thead>
-                   
-                   <th><input type="checkbox" id="checkall" /></th>
-                   <th>Id</th>
-                    <th>Pseudo</th>
-                     <th>Nom</th>
-                     <th>Mail</th>
-                     <th>Date inscription</th>
-                      <th>Edit</th>
-                      
-                       <th>Delete</th>
-                   </thead>
-    <tbody>
-    
-    <tr>
-    <td><input type="checkbox" class="checkthis" /></td>
-    <td> <?php  ?> </td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-    </tr>
-  
-</body>
+       
+
+        
+
+                                
+       
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
